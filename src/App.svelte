234346f1b7bb46df5ca9a3bf86b9ab8a83b63070
Azat Serikov <azatsdev@gmail.com>
2022-04-21 10:35:21 +0600

    init

---
+++ src/App.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  export let name: string;
+</script>
+
+<main>
+  <h1>Hello {name}!</h1>
+  <p class="text-rose-800">Svelte + TypeScript + Tailwind</p>
+</main>
+
+<style lang="postcss" global>
+  @tailwind base;
+  @tailwind components;
+  @tailwind utilities;
+</style>
